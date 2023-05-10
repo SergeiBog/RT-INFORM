86186a7d73e37ecb468161c6747938d0d38bf7c8
Sergey Bogatyrev <kapri44io@gmail.com>
2023-05-10 22:08:17 +0300

    Test

---
+++ src/main/java/org/example/Java.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Java {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNext("count")){
+            list.add(sc.nextInt());
+        }
+        sc.close();
+        HashMap<Integer,Integer> result= countElements(list);
+        for(int key: result.keySet()){
+            System.out.println(key + " : "+result.get(key));
+        }
+    }
+
+    public static HashMap<Integer,Integer> countElements(ArrayList<Integer> list){
+        HashMap<Integer,Integer> result= new HashMap<>();
+        for(int i : list){
+            if(!result.containsKey(i)){
+                result.put(i,1);
+            }else{
+                result.put(i, result.get(i)+1);
+            }
+        }
+        return result;
+    }
+}
