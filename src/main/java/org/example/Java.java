0a149b2bf463e97acdf902d6383025059d8d4d59
Sergey Bogatyrev <kapri44io@gmail.com>
2023-05-10 22:45:43 +0300

    Test

--- src/main/java/org/example/Java.java
+++ src/main/java/org/example/Java.java
@@ -8,24 +8,24 @@ public class Java {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        while (!sc.hasNext("count")){
+        while (!sc.hasNext("count")) {
             list.add(sc.nextInt());
         }
 
         sc.close();
-        HashMap<Integer,Integer> result= countElements(list);
-        for(int key: result.keySet()){
-            System.out.println(key + " : "+result.get(key));
+        HashMap<Integer, Integer> result = countElements(list);
+        for (int key : result.keySet()) {
+            System.out.println(key + " : " + result.get(key));
         }
     }
 
-    public static HashMap<Integer,Integer> countElements(ArrayList<Integer> list){
-        HashMap<Integer,Integer> result= new HashMap<>();
-        for(int i : list){
-            if(!result.containsKey(i)){
-                result.put(i,1);
-            }else{
-                result.put(i, result.get(i)+1);
+    public static HashMap<Integer, Integer> countElements(ArrayList<Integer> list) {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        for (int i : list) {
+            if (!result.containsKey(i)) {
+                result.put(i, 1);
+            } else {
+                result.put(i, result.get(i) + 1);
             }
         }
         return result;
